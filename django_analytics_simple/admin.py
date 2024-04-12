a70437de7e9eeb7a3c4b7f7cf926a0033ff1e9dc
H4cker54n <snbq89@gmail.com>
2024-04-12 09:02:16 +0200

    Update admin.py to add AnalyticsAdmin class for customizing the admin interface

--- django_analytics_simple/admin.py
+++ django_analytics_simple/admin.py
@@ -1,4 +1,9 @@
 from .models import Analytics
 from django.contrib import admin
 
-admin.site.register(Analytics)
+class AnalyticsAdmin(admin.ModelAdmin):
+    list_display = ('browser', 'os', 'device', 'url', 'ip_address', 'country', 'city', 'created_at', 'referer')
+    search_fields = ('browser', 'os', 'device', 'url', 'ip_address', 'country', 'city', 'referer')
+    list_filter = ('browser', 'os', 'device', 'country', 'city')
+
+admin.site.register(Analytics, AnalyticsAdmin)
